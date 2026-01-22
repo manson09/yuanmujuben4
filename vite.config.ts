a0ee8d44a4d8cae70416a10fd8039e045b9ab9e5
manson09 <232405299@qq.com>
2026-01-22 19:20:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-
-      'openai': 'openai/index.mjs'
-    }
-  },
   define: {
- 
+   
     'process.env': {}
   }
 })
